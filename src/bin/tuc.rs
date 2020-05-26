1085674fd2b1616b3208f97cfb54bdf5fdcb8bf3
Riccardo Attilio Galli <riccardo@sideralis.org>
2020-05-26 00:22:11 -0700

    Run cargo fmt

--- src/bin/tuc.rs
+++ src/bin/tuc.rs
@@ -1,22 +1,21 @@
 use anyhow::{bail, Context, Result};
+use regex::Regex;
+use std::fmt;
 use std::io::Read;
 use std::str::FromStr;
-use std::fmt;
 use structopt::StructOpt;
-
+#[macro_use]
+use lazy_static;
 
 #[derive(Debug, StructOpt)]
-#[structopt(
-    name = "tuc",
-    about = "When cut doesn't cut it."
-)]
+#[structopt(name = "tuc", about = "When cut doesn't cut it.")]
 
 struct Opt {
     /// Delimiter to use to cut the text into pieces
-    #[structopt(short, long, default_value="\t")]
+    #[structopt(short, long, default_value = "\t")]
     delimiter: String,
     /// Fields to keep, like 1-3 or 3,2 or 1- or 3,1-2 or -3 or -3--2
-    #[structopt(short, long, default_value="1-")]
+    #[structopt(short, long, default_value = "1-")]
     fields: RangeList,
 }
 
@@ -27,7 +26,7 @@ impl FromStr for RangeList {
     type Err = Box<dyn std::error::Error>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let k: Result<Vec<Range>,_> = s.split(',').map(Range::from_str).collect();
+        let k: Result<Vec<Range>, _> = s.split(',').map(Range::from_str).collect();
         Ok(RangeList(k?))
     }
 }
@@ -37,7 +36,6 @@ enum Side {
     Some(i32),
     Continue,
 }
-
 
 impl fmt::Display for Side {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -60,7 +58,6 @@ impl fmt::Display for Range {
         write!(f, "{}", self.raw)
     }
 }
-
 
 impl FromStr for Range {
     type Err = Box<dyn std::error::Error>;
@@ -87,9 +84,6 @@ impl FromStr for Range {
             }
         }
 
-        let l: Side;
-        let r: Side;
-
         match s[search_start_from..].find('-') {
             Some(k) => {
                 let idx = k + search_start_from;
@@ -97,17 +91,27 @@ impl FromStr for Range {
                 l = if s[..idx].len() == 0 {
                     Side::Continue
                 } else {
-                    Side::Some(s[..idx].parse::<i32>().or_else(|_| Err(format!("Not a number: {}`", s[..idx].to_string())))?)
+                    Side::Some(
+                        s[..idx]
+                            .parse::<i32>()
+                            .or_else(|_| Err(format!("Not a number: {}`", s[..idx].to_string())))?,
+                    )
                 };
 
-                r = if s[idx+1..].len() == 0 {
-                    Side::Continue
-                } else {
-                    Side::Some(s[idx+1..].parse::<i32>().or_else(|_| Err(format!("Not a number: {}`", s[idx+1..].to_string())))?)
-                };
-            },
+                r =
+                    if s[idx + 1..].len() == 0 {
+                        Side::Continue
+                    } else {
+                        Side::Some(s[idx + 1..].parse::<i32>().or_else(|_| {
+                            Err(format!("Not a number: {}`", s[idx + 1..].to_string()))
+                        })?)
+                    };
+            }
             None => {
-                l = Side::Some(s.parse::<i32>().or_else(|_| Err(format!("Not a number `{}`", s)))?);
+                l = Side::Some(
+                    s.parse::<i32>()
+                        .or_else(|_| Err(format!("Not a number `{}`", s)))?,
+                );
                 r = l;
             }
         }
@@ -115,7 +119,7 @@ impl FromStr for Range {
         match (l, r) {
             (Side::Continue, Side::Continue) => {
                 return Err(format!("Error parsing range, no value found in `{}`", s).into());
-            },
+            }
             (Side::Some(0), _) => {
                 return Err("Fields are 1-indexed".into());
             }
@@ -125,17 +129,13 @@ impl FromStr for Range {
             _ => (),
         }
 
-        Ok(Range::new(l,r, s.to_string()))
+        Ok(Range::new(l, r, s.to_string()))
     }
 }
 
 impl Range {
     pub fn new(l: Side, r: Side, raw: String) -> Self {
-        Range {
-            l,
-            r,
-            raw,
-        }
+        Range { l, r, raw }
     }
 }
 
@@ -145,35 +145,38 @@ impl Default for Range {
     }
 }
 
-
 fn main() -> Result<()> {
     let matches = Opt::clap()
         .setting(structopt::clap::AppSettings::AllowLeadingHyphen)
         .get_matches();
 
+    //let opt = Opt::from_args();
     let opt = Opt::from_clap(&matches);
-    println!("{:?}",opt);
+    println!("{:?}", opt);
 
     let mut content = String::new();
     std::io::stdin()
-            .read_to_string(&mut content)
-            .with_context(|| format!("Cannot read from STDIN"))?;
+        .read_to_string(&mut content)
+        .with_context(|| format!("Cannot read from STDIN"))?;
 
     content = content.trim().to_string();
 
     let parts: Vec<&str> = content.split(&opt.delimiter).collect::<Vec<&str>>();
+    //    lazy_static! {
+    //        static ref RE: Regex = Regex::new("^(?P<l>-?\d+)?(?P<sep>:)?(-?\d+)?$").unwrap();
+    //    }
 
     let parts_length = parts.len();
-    let fields_vec = match opt.fields { RangeList(v) => v };
+    let fields_vec = match opt.fields {
+        RangeList(v) => v,
+    };
 
     for f in &fields_vec {
         let l: usize;
         let r: usize;
-        
+
         l = match f.l {
-            Side::Continue => {
-                1
-            }
+            Side::Continue => 1,
             Side::Some(v) => {
                 if v.abs() as usize > parts_length {
                     bail!("Out of bounds: {}", v);
@@ -187,9 +190,7 @@ fn main() -> Result<()> {
         };
 
         r = match f.r {
-            Side::Continue => {
-                parts_length
-            }
+            Side::Continue => parts_length,
             Side::Some(v) => {
                 if v.abs() as usize > parts_length {
                     bail!("Out of bounds: {}", v);
