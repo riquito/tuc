024fdb252577c234c2b14bc4df87a57afb1b09d9
Riccardo Attilio Galli <riccardo@sideralis.org>
2022-05-23 23:39:25 -0700

    Refactor streamed cut_lines to its own function

--- src/cut_lines.rs
+++ src/cut_lines.rs
@@ -7,6 +7,71 @@ use crate::cut_str::cut_str;
 use crate::options::Opt;
 use crate::read_utils::read_line_with_eol;
 
+fn cut_lines_forward_only(
+    stdin: &mut std::io::BufReader<std::io::StdinLock>,
+    stdout: &mut std::io::BufWriter<std::io::StdoutLock>,
+    opt: Opt,
+) -> Result<()> {
+    let mut line_buf = String::with_capacity(1024);
+    let mut line_idx = 0;
+    let mut bounds_idx = 0; // keep track of which bounds have been used
+    let mut add_newline_next = false;
+    while let Some(line) = read_line_with_eol(stdin, &mut line_buf, opt.eol) {
+        line_idx += 1;
+
+        let line = line?;
+        let line: &str = line.as_ref();
+        let line = line.strip_suffix(opt.eol as u8 as char).unwrap_or(line);
+
+        // Print the matching fields. Fields are ordered but can still be
+        // duplicated, e.g. 1-2,2,3 , so we may have to print the same
+        // line multiple times
+        while bounds_idx < opt.bounds.0.len() {
+            let bof = opt.bounds.0.get(bounds_idx).unwrap();
+
+            let b = match bof {
+                BoundOrFiller::Filler(f) => {
+                    stdout.write_all(f.as_bytes())?;
+                    bounds_idx += 1;
+                    continue;
+                }
+                BoundOrFiller::Bound(b) => b,
+            };
+
+            if b.matches(line_idx).unwrap_or(false) {
+                if add_newline_next {
+                    stdout.write_all(&[opt.eol as u8])?;
+                }
+
+                stdout.write_all(line.as_bytes())?;
+                add_newline_next = true;
+
+                if b.r == Side::Some(line_idx) {
+                    // we exhausted the use of that bound, move on
+                    bounds_idx += 1;
+                    add_newline_next = false;
+
+                    // if opt.join and it was not the last matching bound
+                    if opt.join && bounds_idx != opt.bounds.0.len() {
+                        stdout.write_all(&[opt.eol as u8])?;
+                    }
+
+                    continue; // let's see if the next bound matches too
+                }
+            }
+
+            break; // nothing matched, let's go to the next line
+        }
+
+        if bounds_idx == opt.bounds.0.len() {
+            // no need to read the rest, we don't have other bounds to test
+            break;
+        }
+    }
+
+    Ok(())
+}
+
 pub fn read_and_cut_lines(
     stdin: &mut std::io::BufReader<std::io::StdinLock>,
     stdout: &mut std::io::BufWriter<std::io::StdoutLock>,
@@ -25,62 +90,7 @@ pub fn read_and_cut_lines(
     };
 
     if can_be_streamed {
-        let mut line_buf = String::with_capacity(1024);
-        let mut line_idx = 0;
-        let mut bounds_idx = 0; // keep track of which bounds have been used
-        let mut add_newline_next = false;
-        while let Some(line) = read_line_with_eol(stdin, &mut line_buf, opt.eol) {
-            line_idx += 1;
-
-            let line = line?;
-            let line: &str = line.as_ref();
-            let line = line.strip_suffix(opt.eol as u8 as char).unwrap_or(line);
-
-            // Print the matching fields. Fields are ordered but can still be
-            // duplicated, e.g. 1-2,2,3 , so we may have to print the same
-            // line multiple times
-            while bounds_idx < opt.bounds.0.len() {
-                let bof = opt.bounds.0.get(bounds_idx).unwrap();
-
-                let b = match bof {
-                    BoundOrFiller::Filler(f) => {
-                        stdout.write_all(f.as_bytes())?;
-                        bounds_idx += 1;
-                        continue;
-                    }
-                    BoundOrFiller::Bound(b) => b,
-                };
-
-                if b.matches(line_idx).unwrap_or(false) {
-                    if add_newline_next {
-                        stdout.write_all(&[opt.eol as u8])?;
-                    }
-
-                    stdout.write_all(line.as_bytes())?;
-                    add_newline_next = true;
-
-                    if b.r == Side::Some(line_idx) {
-                        // we exhausted the use of that bound, move on
-                        bounds_idx += 1;
-                        add_newline_next = false;
-
-                        // if opt.join and it was not the last matching bound
-                        if opt.join && bounds_idx != opt.bounds.0.len() {
-                            stdout.write_all(&[opt.eol as u8])?;
-                        }
-
-                        continue; // let's see if the next bound matches too
-                    }
-                }
-
-                break; // nothing matched, let's go to the next line
-            }
-
-            if bounds_idx == opt.bounds.0.len() {
-                // no need to read the rest, we don't have other bounds to test
-                break;
-            }
-        }
+        cut_lines_forward_only(stdin, stdout, opt)?;
     } else {
         let mut buffer: Vec<u8> = Vec::with_capacity(32 * 1024);
         stdin.read_to_end(&mut buffer)?;
