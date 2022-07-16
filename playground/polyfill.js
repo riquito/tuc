025d0e551a5eb545ada990845eef4ecd02fb1f7b
Riccardo Attilio Galli <riccardo@sideralis.org>
2022-07-16 16:29:37 -0700

    Remove most debug output

--- playground/polyfill.js
+++ playground/polyfill.js
@@ -1,3 +1,7 @@
+// Modified version of code by Bjorn3
+// Original source: https://github.com/bjorn3/rust/blob/051645c/rustc.html
+// LICENSE: Apache2
+
 // @ts-check
 
 const FILETYPE_UNKNOWN = 0;
@@ -131,7 +135,7 @@ class Directory {
 class PreopenDirectory extends Directory {
   constructor(name, contents) {
     super(contents);
-    this.prestat_name = new TextEncoder('utf-8').encode(name);
+    this.prestat_name = new TextEncoder().encode(name);
   }
 }
 
@@ -155,12 +159,6 @@ class Stdio {
   }
 
   write(buffer) {
-    console.log(
-      'RIQ2',
-      'Class Stdio::write',
-      [new TextDecoder('utf-8').decode(buffer)],
-      [buffer]
-    );
     return this.on_write(buffer);
   }
 }
@@ -176,102 +174,82 @@ class Stdio {
 async function WASM_WASI_instantiate(wasm, args, env, fds) {
   const inst = await WebAssembly.instantiate(wasm, {
     wasi_snapshot_preview1: {
-      proc_exit() {
-        console.log('proc_exit');
+      proc_exit(_n) {
+        return;
       },
       random_get() {
-        console.log('random_get');
+        throw new Error('NotImplemented: random_get');
       },
       args_sizes_get(argc, argv_buf_size) {
         let buffer = new DataView(inst.exports.memory.buffer);
-        console.log('args_sizes_get(', argc, ', ', argv_buf_size, ')');
+
         buffer.setUint32(argc, args.length, true);
         let buf_size = 0;
         for (let arg of args) {
           buf_size += arg.length + 1;
         }
         buffer.setUint32(argv_buf_size, buf_size, true);
-        console.log(
-          buffer.getUint32(argc, true),
-          buffer.getUint32(argv_buf_size, true)
-        );
+
         return 0;
       },
       args_get(argv, argv_buf) {
         let buffer = new DataView(inst.exports.memory.buffer);
         let buffer8 = new Uint8Array(inst.exports.memory.buffer);
-        console.log('args_get(', argv, ', ', argv_buf, ')');
+
         let orig_argv_buf = argv_buf;
         for (let i = 0; i < args.length; i++) {
           buffer.setUint32(argv, argv_buf, true);
           argv += 4;
-          let arg = new TextEncoder('utf-8').encode(args[i]);
+          let arg = new TextEncoder().encode(args[i]);
           buffer8.set(arg, argv_buf);
           buffer.setUint8(argv_buf + arg.length, 0);
           argv_buf += arg.length + 1;
         }
-        console.log(
-          new TextDecoder('utf-8').decode(
-            buffer8.slice(orig_argv_buf, argv_buf)
-          )
-        );
+
         return 0;
       },
 
       environ_sizes_get(environ_count, environ_size) {
         let buffer = new DataView(inst.exports.memory.buffer);
-        console.log(
-          'environ_sizes_get(',
-          environ_count,
-          ', ',
-          environ_size,
-          ')'
-        );
+
         buffer.setUint32(environ_count, env.length, true);
         let buf_size = 0;
         for (let environ of env) {
           buf_size += environ.length + 1;
         }
         buffer.setUint32(environ_size, buf_size, true);
-        console.log(
-          buffer.getUint32(environ_count, true),
-          buffer.getUint32(environ_size, true)
-        );
+
         return 0;
       },
       environ_get(environ, environ_buf) {
         let buffer = new DataView(inst.exports.memory.buffer);
         let buffer8 = new Uint8Array(inst.exports.memory.buffer);
-        console.log('environ_get(', environ, ', ', environ_buf, ')');
+
         let orig_environ_buf = environ_buf;
         for (let i = 0; i < env.length; i++) {
           buffer.setUint32(environ, environ_buf, true);
           environ += 4;
-          let e = new TextEncoder('utf-8').encode(env[i]);
+          let e = new TextEncoder().encode(env[i]);
           buffer8.set(e, environ_buf);
           buffer.setUint8(environ_buf + e.length, 0);
           environ_buf += e.length + 1;
         }
-        console.log(
-          new TextDecoder('utf-8').decode(
-            buffer8.slice(orig_environ_buf, environ_buf)
-          )
-        );
+
         return 0;
       },
 
       clock_time_get(id, precision, time) {
         let buffer = new DataView(inst.exports.memory.buffer);
-        //console.log("clock_time_get(", id, ", ", precision, ", ", time, ")");
+
         buffer.setBigUint64(time, 0n, true);
         return 0;
       },
       fd_close() {
-        console.log('fd_close');
+        throw new Error('NotImplemented: fd_close');
       },
       fd_filestat_get(fd, buf) {
         let buffer = new DataView(inst.exports.memory.buffer);
-        console.warn('fd_filestat_get(', fd, ', ', buf, ')');
+
         if (fds[fd] != undefined) {
           let stat = fds[fd].stat();
           buffer.setBigUint64(buf, stat.dev, true);
@@ -290,17 +268,7 @@ async function WASM_WASI_instantiate(wasm, args, env, fds) {
       fd_read(fd, iovs_ptr, iovs_len, nread_ptr) {
         let buffer = new DataView(inst.exports.memory.buffer);
         let buffer8 = new Uint8Array(inst.exports.memory.buffer);
-        console.log(
-          'fd_read(',
-          fd,
-          ', iovs_ptr',
-          iovs_ptr,
-          ', iovs_len',
-          iovs_len,
-          ', nread_ptr',
-          nread_ptr,
-          ')'
-        );
+
         if (fds[fd] != undefined) {
           buffer.setUint32(nread_ptr, 0, true);
           for (let i = 0; i < iovs_len; i++) {
@@ -309,10 +277,7 @@ async function WASM_WASI_instantiate(wasm, args, env, fds) {
               buffer.getUint32(iovs_ptr + 8 * i + 4, true),
             ];
             let [data, err] = fds[fd].read(len);
-            console.log(`[fd_read] read(len) on fd ${fd} returned`, [
-              data,
-              err,
-            ]);
+
             if (err != 0) {
               return err;
             }
@@ -331,27 +296,11 @@ async function WASM_WASI_instantiate(wasm, args, env, fds) {
       fd_readdir(fd, buf, buf_len, cookie, bufused) {
         let buffer = new DataView(inst.exports.memory.buffer);
         let buffer8 = new Uint8Array(inst.exports.memory.buffer);
-        console.warn(
-          'fd_readdir(',
-          fd,
-          ', ',
-          buf,
-          ', ',
-          buf_len,
-          ', ',
-          cookie,
-          ', ',
-          bufused,
-          ')'
-        );
+
         // 8 ,  3408816 ,  128 ,  0n ,  1032332
         if (fds[fd] != undefined && fds[fd].directory != undefined) {
           buffer.setUint32(bufused, 0, true);
 
-          console.log(
-            cookie,
-            Object.keys(fds[fd].directory).slice(Number(cookie))
-          );
           if (cookie >= BigInt(Object.keys(fds[fd].directory).length)) {
             console.log('end of dir');
             return 0;
@@ -361,8 +310,8 @@ async function WASM_WASI_instantiate(wasm, args, env, fds) {
             Number(cookie)
           )) {
             let entry = fds[fd].directory[name];
-            console.log(name, entry);
-            let encoded_name = new TextEncoder('utf-8').encode(name);
+
+            let encoded_name = new TextEncoder().encode(name);
 
             let offset = 24 + encoded_name.length;
 
@@ -370,37 +319,34 @@ async function WASM_WASI_instantiate(wasm, args, env, fds) {
               console.log('too small buf');
               break;
             } else {
-              console.log('next_cookie =', next_cookie, buf);
               buffer.setBigUint64(buf, next_cookie, true);
               next_cookie += 1n;
               buffer.setBigUint64(buf + 8, 1n, true); // inode
               buffer.setUint32(buf + 16, encoded_name.length, true);
               buffer.setUint8(buf + 20, entry.file_type);
               buffer8.set(encoded_name, buf + 24);
-              console.log('buffer =', buffer8.slice(buf, buf + offset));
+
               buf += offset;
               buffer.setUint32(
                 bufused,
                 buffer.getUint32(bufused, true) + offset,
                 true
               );
-              console.log();
             }
           }
-          console.log('used =', buffer.getUint32(bufused, true));
+
           return 0;
         } else {
           return -1;
         }
       },
       fd_seek() {
-        console.log('fd_seek');
+        throw new Error('NotImplemented: fd_seek');
       },
       fd_write(fd, iovs_ptr, iovs_len, nwritten_ptr) {
-        console.log('I am in fd_write');
         let buffer = new DataView(inst.exports.memory.buffer);
         let buffer8 = new Uint8Array(inst.exports.memory.buffer);
-        //console.log("fd_write(", fd, ", ", iovs_ptr, ", ", iovs_len, ", ", nwritten_ptr, ")");
+
         if (fds[fd] != undefined) {
           buffer.setUint32(nwritten_ptr, 0, true);
           for (let i = 0; i < iovs_len; i++) {
@@ -408,22 +354,8 @@ async function WASM_WASI_instantiate(wasm, args, env, fds) {
               buffer.getUint32(iovs_ptr + 8 * i, true),
               buffer.getUint32(iovs_ptr + 8 * i + 4, true),
             ];
-            console.log(
-              'GOING to write',
-              ptr,
-              len,
-              buffer8.slice(ptr, ptr + len)
-            );
-            console.log('want to write', i, 'out of', iovs_len - 1);
+
             let err = fds[fd].write(buffer8.slice(ptr, ptr + len));
-            console.log(
-              'did write',
-              i,
-              'out of',
-              iovs_len - 1,
-              'got err?',
-              err
-            );
 
             if (err != 0) {
               return err;
@@ -440,29 +372,17 @@ async function WASM_WASI_instantiate(wasm, args, env, fds) {
         }
       },
       path_create_directory() {
-        console.log('path_create_directory');
+        throw new Error('NotImplemented: path_create_directory');
       },
       path_filestat_get(fd, flags, path_ptr, path_len, buf) {
         let buffer = new DataView(inst.exports.memory.buffer);
         let buffer8 = new Uint8Array(inst.exports.memory.buffer);
-        console.warn(
-          'path_filestat_get(',
-          fd,
-          ', ',
-          flags,
-          ', ',
-          path_ptr,
-          ', ',
-          path_len,
-          ', ',
-          buf,
-          ')'
-        );
+
         if (fds[fd] != undefined && fds[fd].directory != undefined) {
           let path = new TextDecoder('utf-8').decode(
             buffer8.slice(path_ptr, path_ptr + path_len)
           );
-          console.log('file =', path);
+
           let entry = fds[fd].get_entry_for_path(path);
           if (entry == null) {
             return -1;
@@ -474,7 +394,7 @@ async function WASM_WASI_instantiate(wasm, args, env, fds) {
         }
       },
       path_link() {
-        console.log('path_link');
+        throw new Error('NotImplemented: path_link');
       },
       path_open(
         fd,
@@ -489,30 +409,12 @@ async function WASM_WASI_instantiate(wasm, args, env, fds) {
       ) {
         let buffer = new DataView(inst.exports.memory.buffer);
         let buffer8 = new Uint8Array(inst.exports.memory.buffer);
-        console.log(
-          'path_open(',
-          dirflags,
-          ', ',
-          path_ptr,
-          ', ',
-          path_len,
-          ', ',
-          oflags,
-          ', ',
-          fs_rights_base,
-          ', ',
-          fs_rights_inheriting,
-          ', ',
-          fdflags,
-          ', ',
-          opened_fd_ptr,
-          ')'
-        );
+
         if (fds[fd] != undefined && fds[fd].directory != undefined) {
           let path = new TextDecoder('utf-8').decode(
             buffer8.slice(path_ptr, path_ptr + path_len)
           );
-          console.log(path);
+
           let entry = fds[fd].get_entry_for_path(path);
           if (entry == null) {
             if (oflags & (OFLAGS_CREAT == OFLAGS_CREAT)) {
@@ -540,23 +442,23 @@ async function WASM_WASI_instantiate(wasm, args, env, fds) {
         }
       },
       path_readlink() {
-        console.log('path_readlink');
+        throw new Error('NotImplemented: path_readlink');
       },
       path_remove_directory() {
-        console.log('path_remove_directory');
+        throw new Error('NotImplemented: path_remove_directory');
       },
       path_rename() {
-        console.log('path_rename');
+        throw new Error('NotImplemented: path_rename');
       },
       path_unlink_file() {
-        console.log('path_unlink_file');
+        throw new Error('NotImplemented: path_unlink_file');
       },
       sched_yield() {
-        console.log('sched_yield');
+        throw new Error('NotImplemented: sched_yield');
       },
       fd_prestat_get(fd, buf_ptr) {
         let buffer = new DataView(inst.exports.memory.buffer);
-        console.log('fd_prestat_get(', fd, ', ', buf_ptr, ')');
+
         if (fds[fd] != undefined && fds[fd].prestat_name != undefined) {
           const PREOPEN_TYPE_DIR = 0;
           buffer.setUint32(buf_ptr, PREOPEN_TYPE_DIR, true);
@@ -567,15 +469,6 @@ async function WASM_WASI_instantiate(wasm, args, env, fds) {
         }
       },
       fd_prestat_dir_name(fd, path_ptr, path_len) {
-        console.log(
-          'fd_prestat_dir_name(',
-          fd,
-          ', ',
-          path_ptr,
-          ', ',
-          path_len,
-          ')'
-        );
         if (fds[fd] != undefined && fds[fd].prestat_name != undefined) {
           let buffer8 = new Uint8Array(inst.exports.memory.buffer);
           buffer8.set(fds[fd].prestat_name, path_ptr);
