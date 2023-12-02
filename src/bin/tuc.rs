29a206fc5786a64c25d7362b2a451e4c17d26929
Riccardo Attilio Galli <riccardo@sideralis.org>
2023-12-02 10:02:33 -0800

    Improve help

--- src/bin/tuc.rs
+++ src/bin/tuc.rs
@@ -41,19 +41,19 @@ OPTIONS:
                                   [default 1:]
 
                                   e.g. cutting on '-' the string 'a-b-c-d'
-                                  1     => a
-                                  1:    => a-b-c-d
-                                  1:3   => a-b-c
-                                  3,2   => cb
-                                  3,1:2 => ca-b
-                                  -3:-2 => b-c
+                                  -f 1     => a
+                                  -f 1:    => a-b-c-d
+                                  -f 1:3   => a-b-c
+                                  -f 3,2   => cb
+                                  -f 3,1:2 => ca-b
+                                  -f -3:-2 => b-c
 
                                   To re-apply the delimiter add -j, to replace
                                   it add -r (followed by the new delimiter).
 
                                   You can also format the output using {} syntax
                                   e.g.
-                                  '["{1}", "{2}"]' => ["a", "b"]
+                                  -f '["{1}", "{2}"]' => ["a", "b"]
 
                                   You can escape { and } using {{ and }}.
 
