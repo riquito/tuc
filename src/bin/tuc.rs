c7b00cc1f53733cb1ebba65cca222c97abbc0de9
Riccardo Attilio Galli <riccardo@sideralis.org>
2024-02-03 01:06:01 -0800

    Initial version of fast lane

--- src/bin/tuc.rs
+++ src/bin/tuc.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
+use std::convert::{TryFrom, TryInto};
 use std::io::Write;
 use std::str::FromStr;
 use tuc::bounds::{BoundOrFiller, BoundsType, UserBoundsList};
 use tuc::cut_bytes::read_and_cut_bytes;
 use tuc::cut_lines::read_and_cut_lines;
 use tuc::cut_str::read_and_cut_str;
+use tuc::fast_lane::{read_and_cut_text_as_bytes, FastOpt};
 use tuc::options::{Opt, EOL};
 
 #[cfg(feature = "regex")]
@@ -264,6 +266,8 @@ fn main() -> Result<()> {
         read_and_cut_bytes(&mut stdin, &mut stdout, &opt)?;
     } else if opt.bounds_type == BoundsType::Lines {
         read_and_cut_lines(&mut stdin, &mut stdout, &opt)?;
+    } else if let Ok(fast_opt) = FastOpt::try_from(&opt) {
+        read_and_cut_text_as_bytes(&mut stdin, &mut stdout, &fast_opt)?;
     } else {
         read_and_cut_str(&mut stdin, &mut stdout, opt)?;
     }
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod bounds;
 pub mod cut_bytes;
 pub mod cut_lines;
 pub mod cut_str;
+pub mod fast_lane;
 mod json;
 pub mod options;
 mod read_utils;
