d73b5fb61f730efcb9f6d730661bf40c3ca4babb
Riccardo Attilio Galli <riccardo@sideralis.org>
2025-08-20 22:18:06 -0700

    Add tests for RegexFinder

--- src/finders/regex.rs
+++ src/finders/regex.rs
@@ -8,7 +8,11 @@ pub struct RegexFinder {
 }
 
 impl RegexFinder {
-    pub fn new(regex: Regex, trim_empty: bool) -> Self {
+    pub fn new(regex: Regex) -> Self {
+        Self::new_with_trim(regex, false)
+    }
+
+    pub fn new_with_trim(regex: Regex, trim_empty: bool) -> Self {
         Self { regex, trim_empty }
     }
 }
@@ -31,5 +35,90 @@ impl DelimiterFinder for RegexFinder {
         } else {
             Box::new(self.regex.find_iter(line).map(|m| m.start()..m.end()))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+    use std::sync::LazyLock;
+
+    use super::*;
+
+    static SMALL_DELIM_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::from_str("[-]").unwrap());
+    static BIG_DELIM_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::from_str("-{2}").unwrap());
+
+    #[test]
+    fn forward_small_delimiter_empty_line() {
+        let finder = RegexFinder::new(SMALL_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"").collect();
+        assert_eq!(Vec::<Range<usize>>::new(), result);
+    }
+
+    #[test]
+    fn forward_small_delimiter_only_delimiter() {
+        let finder = RegexFinder::new(SMALL_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"-").collect();
+        assert_eq!(vec![0..1], result);
+    }
+
+    #[test]
+    fn forward_small_delimiter_regular_scenarios() {
+        let finder = RegexFinder::new(SMALL_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"a").collect();
+        assert_eq!(Vec::<Range<usize>>::new(), result);
+
+        let finder = RegexFinder::new(SMALL_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"a-b").collect();
+        assert_eq!(vec![1..2], result);
+
+        let finder = RegexFinder::new(SMALL_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"a-b-c").collect();
+        assert_eq!(vec![1..2, 3..4], result);
+
+        let finder = RegexFinder::new(SMALL_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"a-b-c-").collect();
+        assert_eq!(vec![1..2, 3..4, 5..6], result);
+
+        let finder = RegexFinder::new(SMALL_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"-a-b-c").collect();
+        assert_eq!(vec![0..1, 2..3, 4..5], result);
+    }
+
+    #[test]
+    fn forward_big_delimiter_empty_line() {
+        let finder = RegexFinder::new(BIG_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"").collect();
+        assert_eq!(Vec::<Range<usize>>::new(), result);
+    }
+
+    #[test]
+    fn forward_big_delimiter_only_delimiter() {
+        let finder = RegexFinder::new(BIG_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"--").collect();
+        assert_eq!(vec![0..2], result);
+    }
+
+    #[test]
+    fn forward_big_delimiter_regular_scenarios() {
+        let finder = RegexFinder::new(BIG_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"a").collect();
+        assert_eq!(Vec::<Range<usize>>::new(), result);
+
+        let finder = RegexFinder::new(BIG_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"a--b").collect();
+        assert_eq!(vec![1..3], result);
+
+        let finder = RegexFinder::new(BIG_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"a--b--c").collect();
+        assert_eq!(vec![1..3, 4..6], result);
+
+        let finder = RegexFinder::new(BIG_DELIM_REGEX.clone());
+        let result: Vec<Range<usize>> = finder.find_ranges(b"a--b--c--").collect();
+        assert_eq!(vec![1..3, 4..6, 7..9], result);
+        let finder = RegexFinder::new(BIG_DELIM_REGEX.clone());
+
+        let result: Vec<Range<usize>> = finder.find_ranges(b"--a--b--c").collect();
+        assert_eq!(vec![0..2, 3..5, 6..8], result);
     }
 }
